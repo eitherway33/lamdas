5811b7bcc68f05f202851b97f64a8c11152b1ee7
eitherway33 <salvador.estrada08@gmail.com>
2020-04-18 23:00:21 -0500

    se implementó método existencia <10

--- Lamdas/src/salvador/lamdas/Main.java
+++ Lamdas/src/salvador/lamdas/Main.java
@@ -14,11 +14,22 @@ public class Main {
     return stream.collect(Collectors.toList());
    } 
 
+   //MUESTRA LOS NOMBRES DEL PRODUCTO QUE TENGAN UNA EXISTENCIA MENOR A 10 EN EL ALMACEN 
+
+   public List<String> existMenor10()
+   {
+       List<Product> listProd = Util.getProducts(); 
+       Stream<String> stream = listProd.stream().filter(p->p.getUnitInStock()<10).map(Product::getName); 
+       return stream.collect(Collectors.toList());
+   }
+
 
    public static void main(String args[]) 
    {
        Main m = new Main(); 
-       m.nomProduct().forEach(x-> System.out.println(x));
-   }
+      // m.nomProduct().forEach(x-> System.out.println(x)); 
+       m.existMenor10().forEach(p->System.out.println(p));
+   } 
+
    
 }
